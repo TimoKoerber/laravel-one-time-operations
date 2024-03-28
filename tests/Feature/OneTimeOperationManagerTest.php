9c48f37ecd660bd2abeda41954185cf2432cadfc
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2024-03-28 18:33:32 -0500

    wip

--- tests/Feature/OneTimeOperationManagerTest.php
+++ tests/Feature/OneTimeOperationManagerTest.php
@@ -1,184 +1,94 @@
 <?php
 
-namespace EncoreDigitalGroup\LaravelOperations\Tests\Feature;
+use Illuminate\Support\Facades\Artisan;
+use function Pest\Laravel\artisan;
+use Illuminate\Support\Facades\File;
 
-use EncoreDigitalGroup\LaravelOperations\LaravelOperation;
-use EncoreDigitalGroup\LaravelOperations\LaravelOperationFile;
-use EncoreDigitalGroup\LaravelOperations\LaravelOperationManager;
-use EncoreDigitalGroup\LaravelOperations\Models\Operation;
-use Illuminate\Contracts\Filesystem\FileNotFoundException;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Collection;
-use SplFileInfo;
+it('operations_show_command', function () {
+    // no files found
+    Artisan::call('operations:show')
+        ->expectsOutput('No operations found.');
 
-class OneTimeOperationManagerTest extends OneTimeOperationCase
+    // create operations
+    artisan('operations:make FooBarOperation')->assertSuccessful();
+
+    // no files found
+    artisan('operations:show')
+        ->expectsOutput('2015_10_21_072800_foo_bar_operation');
+
+    // PENDING
+    artisan('operations:process')->assertSuccessful();
+
+    artisan('operations:show')
+        ->expectsOutput('2015_10_21_072800_foo_bar_operation');
+
+    // PROCESSED
+    // create operations
+    artisan('operations:make AwesomeOperation')->assertSuccessful();
+
+    // new pending operation added
+    artisan('operations:show')
+        ->expectsOutput('2015_10_21_072800_foo_bar_operation') // PROCESSED
+        ->expectsOutput('2015_10_21_072800_awesome_operation');
+
+    // PENDING
+    artisan('operations:process')->assertSuccessful();
+
+    // create operations
+    artisan('operations:make SuperiorOperation')->assertSuccessful();
+
+    // seconds operation was processed, third operation was added
+    artisan('operations:show')
+        ->expectsOutput('2015_10_21_072800_foo_bar_operation') // PROCESSED
+        ->expectsOutput('2015_10_21_072800_awesome_operation') // PROCESSED
+        ->expectsOutput('2015_10_21_072800_superior_operation');
+
+    // PENDING
+    // delete first file
+    File::delete(filepath('2015_10_21_072800_foo_bar_operation.php'));
+
+    artisan('operations:show')
+        ->expectsOutput('2015_10_21_072800_foo_bar_operation') // DISPOSED
+        ->expectsOutput('2015_10_21_072800_awesome_operation') // PROCESSED
+        ->expectsOutput('2015_10_21_072800_superior_operation');
+
+    // PENDING
+    // filter disposed
+    artisan('operations:show disposed')
+        ->expectsOutput('2015_10_21_072800_foo_bar_operation') // DISPOSED
+        ->doesntExpectOutput('2015_10_21_072800_awesome_operation') // PROCESSED
+        ->doesntExpectOutput('2015_10_21_072800_superior_operation');
+
+    // PENDING
+    // filter processed
+    artisan('operations:show processed')
+        ->doesntExpectOutput('2015_10_21_072800_foo_bar_operation') // DISPOSED
+        ->expectsOutput('2015_10_21_072800_awesome_operation') // PROCESSED
+        ->doesntExpectOutput('2015_10_21_072800_superior_operation');
+
+    // PENDING
+    // filter pending
+    artisan('operations:show pending')
+        ->doesntExpectOutput('2015_10_21_072800_foo_bar_operation') // DISPOSED
+        ->doesntExpectOutput('2015_10_21_072800_awesome_operation') // PROCESSED
+        ->expectsOutput('2015_10_21_072800_superior_operation');
+
+    // PENDING
+    // filter pending
+    artisan('operations:show stuff')
+        ->assertFailed()
+        ->expectsOutput('Given filter is not valid. Allowed filters: pending|processed|disposed.');
+});
+
+function filepath(string $filename): string
 {
-    use RefreshDatabase;
+    return base_path(config('operations.directory')) . DIRECTORY_SEPARATOR . $filename;
+}
 
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->mockFileDirectory();
-    }
-
-    protected function tearDown(): void
-    {
-        $this->deleteFileDirectory();
-    }
-
-    /**
-     * @test
-     */
-    public function get_directory_path()
-    {
-        $this->assertStringEndsWith('tests/files/', LaravelOperationManager::getDirectoryPath()); // path was set by self::mockDirectory()
-    }
-
-    /**
-     * @test
-     */
-    public function get_path_to_file_by_name()
-    {
-        $this->assertStringEndsWith('/tests/files/narfpuit.php', LaravelOperationManager::pathToFileByName('narfpuit'));
-        $this->assertStringEndsWith('/tests/files/20220101_223355_foobar.php', LaravelOperationManager::pathToFileByName('20220101_223355_foobar'));
-    }
-
-    /**
-     * @test
-     */
-    public function build_filename()
-    {
-        $this->assertEquals('foo.php', LaravelOperationManager::buildFilename('foo'));
-        $this->assertEquals('bar.php', LaravelOperationManager::buildFilename('bar'));
-    }
-
-    /**
-     * @test
-     */
-    public function get_operation_name_from_filename()
-    {
-        $this->assertEquals('20220223_foo', LaravelOperationManager::getOperationNameFromFilename('20220223_foo.php'));
-        $this->assertEquals('20220223_bar', LaravelOperationManager::getOperationNameFromFilename('20220223_bar.php'));
-    }
-
-    /**
-     * @test
-     */
-    public function get_table_name()
-    {
-        $this->assertEquals('operations', LaravelOperationManager::getTableName()); // was set in parent::mockTable();
-    }
-
-    /**
-     * @test
-     */
-    public function get_operation_file_by_model()
-    {
-        $operationModel = Operation::factory()->make(['name' => self::TEST_OPERATION_NAME]);
-
-        $operationFile = LaravelOperationManager::getOperationFileByModel($operationModel);
-
-        $this->assertInstanceOf(LaravelOperationFile::class, $operationFile);
-        $this->assertInstanceOf(LaravelOperation::class, $operationFile->getClassObject());
-        $this->assertEquals($operationModel->name, $operationFile->getOperationName());
-    }
-
-    /**
-     * @test
-     */
-    public function get_operation_file_by_model_throws_exception()
-    {
-        $operationModel = Operation::factory()->make(['name' => 'file_does_not_exist']); // matching file does noe exist
-
-        $this->expectException(FileNotFoundException::class);
-
-        LaravelOperationManager::getOperationFileByModel($operationModel);
-    }
-
-    /**
-     * @test
-     */
-    public function get_class_object_by_name_missing_file()
-    {
-        $this->expectException(FileNotFoundException::class);
-
-        LaravelOperationManager::getClassObjectByName('file_does_not_exist');
-    }
-
-    /**
-     * @test
-     */
-    public function get_class_object_by_name()
-    {
-        $operationClass = LaravelOperationManager::getClassObjectByName(self::TEST_OPERATION_NAME);
-
-        $this->assertInstanceOf(LaravelOperation::class, $operationClass);
-    }
-
-    /**
-     * @test
-     */
-    public function get_all_operation_files()
-    {
-        $files = LaravelOperationManager::getAllFiles();
-
-        /** @var SplFileInfo $firstFile */
-        $firstFile = $files->first();
-        /** @var SplFileInfo $secondFile */
-        $secondFile = $files->last();
-
-        $this->assertInstanceOf(Collection::class, $files);
-        $this->assertCount(2, $files);
-
-        $this->assertInstanceOf(SplFileInfo::class, $firstFile);
-        $this->assertEquals('xxxx_xx_xx_xxxxxx_foo_bar.php', $firstFile->getBasename());
-
-        $this->assertInstanceOf(SplFileInfo::class, $secondFile);
-        $this->assertEquals('xxxx_xx_xx_xxxxxx_narf_puit.php', $secondFile->getBasename());
-    }
-
-    /**
-     * @test
-     */
-    public function get_unprocessed_files()
-    {
-        $files = LaravelOperationManager::getUnprocessedFiles();
-
-        /** @var SplFileInfo $firstFile */
-        $firstFile = $files->first();
-
-        $this->assertInstanceOf(Collection::class, $files);
-        $this->assertCount(2, $files);
-
-        $this->assertInstanceOf(SplFileInfo::class, $firstFile);
-
-        // create entry for file #1 -> file is processed
-        Operation::storeOperation('xxxx_xx_xx_xxxxxx_foo_bar', true);
-
-        $files = LaravelOperationManager::getUnprocessedFiles();
-        $this->assertCount(1, $files);
-
-        // create entry for file #2 -> file is processed
-        Operation::storeOperation('xxxx_xx_xx_xxxxxx_narf_puit', false);
-
-        $files = LaravelOperationManager::getUnprocessedFiles();
-        $this->assertCount(0, $files);
-    }
-
-    /**
-     * @test
-     */
-    public function get_unprocessed_operation_files()
-    {
-        $files = LaravelOperationManager::getUnprocessedOperationFiles();
-
-        $this->assertInstanceOf(Collection::class, $files);
-        $this->assertCount(2, $files);
-
-        /** @var SplFileInfo $firstFile */
-        $firstFile = $files->first();
-
-        $this->assertInstanceOf(LaravelOperationFile::class, $firstFile);
-    }
+function editFile(string $filename, string $search, string $replace)
+{
+    $filepath = filepath($filename);
+    $fileContent = File::get($filepath);
+    $newContent = Str::replaceFirst($search, $replace, $fileContent);
+    File::put($filepath, $newContent);
 }
